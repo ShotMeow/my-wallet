1a0e84849b9c79c7893cf648bb27ece1534f7b72
ShotMeow <shotmeow@gmail.com>
2022-09-14 21:50:43 +0300

    Confirm Main Component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { FC } from 'react'
+import Header from '../Header/Header'
+import CardsList from '../CardsList/CardsList'
+import History from '../History/History'
+import './App.css'
 
-const App = () => {
-	return <section></section>
+const App: FC = () => {
+	return (
+		<section className="app">
+			<div className="container">
+				<Header />
+
+				<div className="grid">
+					<CardsList />
+					<History />
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default App
